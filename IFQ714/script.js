61f92f8d59c848b0241dfc7f677e981564fd205f
Ebad Salehi <esalehi@instructors.2u.com>
2024-07-30 00:51:55 +1000

    branch updates

--- IFQ714/script.js
+++ IFQ714/script.js
@@ -1,3 +1,11 @@
 //This is a test file
 console.log("Testing…");
 console.log("Done!");
+
+//This is a test file
+const branch = true;
+console.log("Testing…");
+if (branch) {
+    console.log("This is part of the new branch.");
+}
+console.log("Done!");
